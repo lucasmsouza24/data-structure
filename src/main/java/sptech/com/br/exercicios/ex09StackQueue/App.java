d2c9efe825436db69bef056a792755de58be8594
Lucas Mesquita de Souza <79462136+lucasmsouza24@users.noreply.github.com>
2022-05-22 22:04:06 -0300

    Update App.java

--- src/main/java/sptech/com/br/exercicios/ex09StackQueue/App.java
+++ src/main/java/sptech/com/br/exercicios/ex09StackQueue/App.java
@@ -1,5 +1,20 @@
 package sptech.com.br.exercicios.ex09StackQueue;
 
 public class App {
-    
+    public static void main(String[] args) {
+        
+        // a
+        MyStack<Operacao> pilha = new MyStack<Operacao>(10);
+        MyQueue<Operacao> fila = new MyQueue<Operacao>(10);
+
+        // b
+        int operacoesRealizadas = 0;
+
+        // c
+        ContaBancaria c1 = new ContaBancaria(100, 4500d);
+        ContaBancaria c2 = new ContaBancaria(200, 3000d);
+
+        // d
+        
+    }
 }
